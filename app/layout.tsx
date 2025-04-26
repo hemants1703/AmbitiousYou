c6fd74c9909121183daac5df7591275640eb51b2
Hemant Sharma <hemant@hemantsharma.tech>
2025-04-26 22:54:05 +0530

    version: before auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
       <body className="antialiased">
         <Analytics />
         <ThemeProvider attribute="class" defaultTheme="system">
