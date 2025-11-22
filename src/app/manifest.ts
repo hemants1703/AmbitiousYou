8e60cc16aa14c1b87e577f3931e0d1ade04dd444
hemants1703 <hemant@hemantsharma.tech>
2025-11-23 01:45:36 +0530

    Add app icons and manifest for PWA support
    Added various icon files (PNG, SVG, ICO) and a manifest.ts file to support web app manifest requirements for PWA functionality. Updated favicon.ico and included new icons for Apple and general use.

---
+++ src/app/manifest.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "AmbitiousYou",
+    short_name: "AmbitiousYou",
+    description:
+      "Reduce your mental overload and manage all your ambitions at a single place, AmbitiousYou helps you become a Superhuman!",
+    start_url: "/dashboard",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  };
+}
