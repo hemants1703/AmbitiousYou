c8812671757dea10e184b99c230dd0e4d86fddec
hemants1703 <hemant@hemantsharma.tech>
2025-11-22 23:31:58 +0530

    add: util to get fieldErrors for the validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// Utility function to get the error message for a field from the validation errors
+export function getFieldError(formErrors: Record<string, string[]>, field: string) {
+  return formErrors?.[field]?.join(", ");
+}
