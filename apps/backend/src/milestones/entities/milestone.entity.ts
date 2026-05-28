298855f56efea12f3fefc50e3fa73eed984bcefb
hemants1703 <hemant@hemantsharma.tech>
2026-05-28 20:04:59 +0530

    fix(milestone): specify table name for MilestoneEntity

--- apps/backend/src/milestones/entities/milestone.entity.ts
+++ apps/backend/src/milestones/entities/milestone.entity.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('milestones')
 export class MilestoneEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string = '';
