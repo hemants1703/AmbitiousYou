300232b3bbddc42f7d143b2a7eeaf054d488762b
hemants1703 <hemant@hemantsharma.tech>
2026-05-28 20:07:13 +0530

    feat(milestone): create CreateMilestoneDto with validation decorators

--- apps/backend/src/milestones/dto/create-milestone.dto.ts
+++ apps/backend/src/milestones/dto/create-milestone.dto.ts
@@ -1,1 +1,33 @@
-export class CreateMilestoneDto {}
+import { NewMilestone } from '@ambitiousyou/shared';
+import { IsBoolean, IsDate, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateMilestoneDto implements NewMilestone {
+  @IsString()
+  @IsNotEmpty()
+  userId: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  ambitionId: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  milestone: string = '';
+
+  @IsString()
+  milestoneDescription: string = '';
+
+  @IsBoolean()
+  @IsBoolean()
+  milestoneCompleted: boolean = false;
+
+  @IsDate()
+  @IsNotEmpty()
+  milestoneTargetDate: Date = new Date();
+
+  @IsDate()
+  createdAt: Date = new Date();
+
+  @IsDate()
+  updatedAt: Date = new Date();
+}
